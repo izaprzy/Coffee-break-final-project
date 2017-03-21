8e983a176e94be08cb546974985fe91a575970d2
Izabela Przybyło <izabelaprzybylo@gmail.com>
2017-03-21 17:30:49 +0100

    Nav animated

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ $(function () {
     console.log('DOM działa');
     var $links = $('section.links');
     var $hamburger = $('a.menu-overlay');
-    var $topLineHamburger =$('p.one');
-    var $middleLineHamburger =$('p.two');
-    var $bottomLineHamburger =$('p.three');
+
+    var $topLineHamburger =$('p.menu-overlay-line.one');
+    var $middleLineHamburger =$('p.menu-overlay-line.two');
+    var $bottomLineHamburger =$('p.menu-overlay-line.three');
+
     var $nav = $('nav.navigation');
     $hamburger.on('click', function () {
         $middleLineHamburger.toggleClass('hidden');
-        $topLineHamburger.toggleClass('.transform-line1').toggleClass('redCloseButton');
-        $bottomLineHamburger.toggleClass('.transform-line3').toggleClass('redCloseButton');
-        $nav.toggleClass('hidden').toggleClass('navigationAnimate');
+        $topLineHamburger.toggleClass('transform-line1').toggleClass('redCloseButton');
+        $bottomLineHamburger.toggleClass('transform-line3').toggleClass('redCloseButton');
+        $nav.toggleClass('hidden').toggleClass('navigationOpen');
         $links.toggleClass('hidden');
     });
 });
