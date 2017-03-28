e127d25e5d11f4dfba9a6338fc49577632f65db8
Izabela Przybyło <izabelaprzybylo@gmail.com>
2017-03-28 19:55:47 +0200

    nav-bar changed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(function () {
+    // $(window).load(function(){
+    //     $('#preloader').fadeOut('slow',function(){$(this).remove();});
+    // });
     console.log('DOM działa');
-
-
 
     //Main page
     var $links = $('section.links');
@@ -21,33 +22,29 @@ $(function () {
 
             //first if - after loading page
             if ( $nav.hasClass('hidden')) {
-                $nav.removeClass('hidden').addClass('navigationOpen');
+                $nav.removeClass('hidden').columnMiddle('navigationOpen');
                 // $nav.toggleClass('hidden').removeClass('navigationClose');
             } else if ($nav.hasClass('navigationOpen')){
                 $nav.removeClass('navigationOpen');
-                $nav.addClass('navigationClose');
+                $nav.columnMiddle('navigationClose');
             } else {
                 $nav.removeClass('navigationClose');
-                $nav.addClass('navigationOpen');
+                $nav.columnMiddle('navigationOpen');
             }
 
             $links.fadeToggle(1000);
         });
     }
-
     navigationMenuAnimate();
 
 
 
-
     //flickr-Api
-
-    var closeButton = $('i.fa.fa-times');
     var div = $('section.photo');
     var form = $('form.search');
     var input = form.find('input');
     var resultsNumber = $('span.resultsNumber');
-    // console.log(resultsNumber);
+    var starWithFavourites = $('i.all-favourites');
 
     function insertPhotoWithQuote(photos) {
 
@@ -65,10 +62,10 @@ $(function () {
             // console.log($img.data('tags')); // prints tags
             var $addPhoto = $('<div>', {'class': 'bottomSpace'}).append($img);
 
-            var favouriteHeart = $('<i>', { 'class':'fa fa-heart fa-2x favouriteHeart'}, { 'aria-hidden':'true' }).css('position', 'absolute'); // heart font
-            // var favouriteHeart = $('<button>', { 'class': 'favouriteHeart'}).append(heart).css('position', 'absolute'); // heart font onside submit button
+            var favouriteStar = $('<i>', { 'class':'fa fa-star fa-2x favouriteStar'}, { 'aria-hidden':'true' }).css('position', 'absolute'); // heart font
+            // var favouriteStar = $('<button>', { 'class': 'favouriteStar'}).append(heart).css('position', 'absolute'); // heart font onside submit button
 
-            $addPhoto.append(favouriteHeart);
+            $addPhoto.append(favouriteStar);
 
             if (index < 170) {
                 // console.log(url);
@@ -78,29 +75,54 @@ $(function () {
             } else {
                 columnRight.append($addPhoto);
             }
-
+            // $(window).load(function() { // czekamy, aż załaduje się cała strona
+            //     $('#status').fadeOut(); // efekt zanikania animowanej grafiki
+            //     $('#preloader').delay(350).fadeOut('slow'); // efekt zanikania warstwy zakrywającej stronę
+            //     $('body').delay(350).css({'overflow':'visible'}); // dopóki nasz div#preloader jest widoczny nie ma możliwości przewijania strony
+            // });
         });
 
         function saveFavourites() {
-            var fav;
+            var fav ='';
+            var saved = $('i.favouriteStar');
             if ( !localStorage.getItem('favs') ){
                 var favs = [];
+                console.log('pusto');
             } else {
                 var favs = JSON.parse( localStorage.getItem('favs') );
+                localStorage.removeItem("favs");
+                console.log(favs, 'tutaj');
             }
-            $('div.bottomSpace').on('click', 'i.favouriteHeart', function () {
-                console.log($(this).prev().attr('src'));
+            $('div.bottomSpace').on('click', 'i.favouriteStar', function () {
+                starWithFavourites.addClass('saved');
+                $(this).addClass('rotate-scale-up').addClass('saved');
                 fav = $(this).prev().attr('src');
-                favs.push(fav);
-                localStorage.setItem("favs", JSON.stringify(favs));
-                var test = JSON.stringify(favs);
-                console.log(test);
+                if ( favs.indexOf(fav) < 0) {
+                    favs.push(fav);
+                    localStorage.setItem( 'favs', JSON.stringify( favs ));
+                    console.log(JSON.parse(localStorage.getItem('favs')));
+                }
+
+                starWithFavourites.on('click', function () {
+                    // localStorage.removeItem("favs");
+                    starWithFavourites.removeClass('saved');
+                    saved.removeClass('saved');
+                    var favs = JSON.parse( localStorage.getItem('favs') );
+                    // localStorage.removeItem("favs");
+                    localStorage.removeItem(JSON.parse(localStorage.getItem('favs')));
+                    console.log(favs);
+                })
             });
 
 
+
+
+            // $('i.fa.fa-star-o.fa-2x').on('click',function () {
+            //     $(this).
+            //
+            // })
+
         }
-
-
 //         //Pobierać:
 //         if ( !localStorage.get('favs') ){
 //             var favs = [];
@@ -110,56 +132,52 @@ $(function () {
 // //Zapisywać:
 //         favs to są ulubione
 //         localStorage.set( 'favs', JSON.stringify( favs ) );
-
-
-
-
         saveFavourites();
+
+
         function findPhotoWithTag() {
-
-
             input.on('focus',function () {
                 $(this).val('');
             });
 
             form.on('submit', function (e) {
                 e.preventDefault();
-                var tag = input.val();
+                var tag = input.val().toLowerCase();
                 var counter = 0;
                 $('img').parent().each(function() {
                     if ( $(this).hasClass('hidden') ) { // before searching I make visible all images
                         $(this).removeClass('hidden');
                     }
                     if ( !tag ) { // if user didnt put any tag
-                        resultsNumber.text('no tag suggestion').addClass('warning');
+                        resultsNumber.text('no tag suggestion');
                     } else if ( $(this).children().data('tags').indexOf(tag) < 0 ) {
                         counter++;
                         $(this).addClass('hidden');
                         var number = 500 - counter;
                         resultsNumber.text('You have ' + number + ' results');
                     }
+                    $('html,body').scrollTop(0); // after searching it come back to the top
                 })
             });
         }
-
-        var test = { test: "thing", test2: "thing2", test3: [0, 2, 44] };
-        localStorage.setItem("test", JSON.stringify(test));
-        var test2 = localStorage.getItem("test");
-        console.log(test2); //Logs "{"test":"thing","test2":"thing2","test3":[0,2,44]}"
-
         findPhotoWithTag();
-
-
-
+        // var test = { one: "thing", two: "thing2", three: [0, 2, 44] };
+        // var test2 = { 1: "hmhmhm"};
+        // var test3 = { kupa: "jeża"};
+        //
+        // localStorage.setItem("test", JSON.stringify(test));
+        // localStorage.setItem("test2", JSON.stringify(test2));
+        // localStorage.setItem("test3", JSON.stringify(test3));
+        // // var test2 = localStorage.getItem("test");
+        // console.log(localStorage["test2"]); //Logs "{"test":"thing","test2":"thing2","test3":[0,2,44]}"
 
 
     }
 
     $.ajax({
-        url: "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=3a8ae2ce4481865b5e7de17a2fbce311&group_id=564487%40N23&extras=tags%2Curl_o&per_page=500&page=1&format=json&nojsoncallback=1"
+        url: "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=831f64fdf59ecfa7a52f1d3cb90087c5&group_id=564487%40N23&extras=tags%2Curl_o&per_page=500&page=1&format=json&nojsoncallback=1"
     }).done(function (response) {
         console.log(response);
-        // console.log(response.photos.photo);
 
         insertPhotoWithQuote(response.photos.photo);
 
